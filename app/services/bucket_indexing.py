eb7cf607390c26ff9eaf5c8aeaffc6d020359dce
João Pedro Pinheiro <joao.pinheiro@olxbr.com>
2022-11-11 21:45:23 -0300

    Adjusting pattern to filter keys and renaming fields

--- app/services/bucket_indexing.py
+++ app/services/bucket_indexing.py
@@ -3,7 +3,7 @@ from app.helper import create_index
 from redis.commands.search.query import Query
 
 
-def delete(pattern="txt_*"):
+def delete(pattern="txt:*"):
     keys = sc.api_redis_cli.keys(pattern)
     for key in keys:
         sc.api_logger.info(f"dropping key {key}")
@@ -11,7 +11,7 @@ def delete(pattern="txt_*"):
     sc.api_logger.info(f"{len(keys)} keys dropped")
 
 
-def run(pattern="txt-*"):
+def run(pattern="txt:*"):
     keys = sc.api_redis_cli.keys(pattern)
     bucket_size = [0, 0, 0, 0, 0]
     for key in keys:
@@ -20,9 +20,9 @@ def run(pattern="txt-*"):
         bucket_size[bucket] += 1
         obj = sc.api_redis_cli.hgetall(key)
         sc.api_redis_cli.hset(
-            f"txt_{bucket}-{new_key}",
+            f"txt:{bucket}:{new_key}",
             mapping={
-                f"embedding_{bucket}": obj[b"embedding"],
+                "embedding": obj[b"embedding"],
                 "sentence": obj[b"sentence"],
                 "id": key
             }
@@ -33,7 +33,7 @@ def run(pattern="txt-*"):
         create_index(
             index_name=f"idx_txt_{bucket}",
             distance_metric=sc.TEXT_DISTANCE_METRIC,
-            vector_field_name=f"embedding_{bucket}",
+            vector_field_name="embedding",
             embedding_dimension=sc.TEXT_EMBEDDING_DIMENSION,
             number_of_vectors=bucket_size[bucket],
             index_type="HNSW",
@@ -45,7 +45,7 @@ def query(kws="iPhone", k=20):
     query_vector = sc.model_txt.encode(kws).astype(sc.TEXT_EMBEDDING_TYPE).tobytes()
     # FLAT - 1k docs
     q = Query(f"*=>[KNN {k} @embedding $query_vector AS score]")\
-        .sort_by("score", asc=False)\
+        .sort_by("score", asc=True)\
         .return_fields("id", "sentence", "score")\
         .dialect(2)
     params_dict = {"query_vector": query_vector}
@@ -53,7 +53,7 @@ def query(kws="iPhone", k=20):
     # HNSW - 5 buckets - ~200 docs each
     bucket_results = [[], [], [], [], []]
     for bucket in range(sc.BUCKETS):
-        q = Query(f"*=>[KNN {k // sc.BUCKETS} @embedding_{bucket} $query_vector AS score]")\
+        q = Query(f"*=>[KNN {k // sc.BUCKETS} @embedding $query_vector AS score]")\
             .sort_by("score", asc=True)\
             .return_fields("id", "sentence", "score")\
             .dialect(2)
